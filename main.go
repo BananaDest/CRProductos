d77b49836383e4158355f8c61e9cb4b3e1a13361
BananaDest <famega12@gmail.com>
2024-09-28 20:08:43 -0600

    feat(api): added http handlers for post put patch delete

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 	_ "github.com/lib/pq"
+	"reflect"
+	"strings"
 )
 
 const (
@@ -85,7 +87,30 @@ func newServer(db *sql.DB) *Server {
 	return &Server{DB: db}
 }
 
-func (s *Server) getAllProducts(w http.ResponseWriter, r *http.Request) {
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db := connectToPostgres(psqlInfo)
+	defer db.Close()
+	server := newServer(db)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Get("/", rootHandler)
+	r.Route("/products", func(r chi.Router) {
+		r.Get("/", server.GetAllProducts)
+		r.Get("/{id}", server.GetProductById)
+		r.Post("/", server.CreateProduct)
+		r.Put("/{id}", server.UpdateProduct)
+		r.Delete("/{id}", server.DeleteProduct)
+		r.Patch("/{id}", server.PatchProduct)
+	})
+	http.ListenAndServe(":8080", r)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
+}
+func (s *Server) GetAllProducts(w http.ResponseWriter, r *http.Request) {
 	rows, err := s.DB.Query("select id,\"name\",quantity,unit,stores from product")
 	if err != nil {
 		http.Error(w, "Failed to query product", http.StatusInternalServerError)
@@ -113,29 +138,164 @@ func (s *Server) getAllProducts(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, products)
 
 }
-func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db := connectToPostgres(psqlInfo)
-	defer db.Close()
-	server := newServer(db)
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
-	r.Get("/", rootHandler)
-	r.Route("/products", func(r chi.Router) {
-		r.Get("/", server.getAllProducts)
-		// r.Get("/{id}", getProductByIdHandler)
-		// r.Post("/", createProductHandler)
-		// r.Put("/{id}", updateProductHandler)
-		// r.Delete("/{id}", deleteProductHandler)
-	})
-	http.ListenAndServe(":8080", r)
+func (s *Server) GetProductById(w http.ResponseWriter, r *http.Request) {
+	var id = chi.URLParam(r, "id")
+	var product Product
+	if err := s.DB.QueryRow("select * from product where product.id = $1", id).Scan(&product.id, &product.name, &product.quantity, &product.unit, &product.stores); err != nil {
+		log.Println("failed to scan: ", err)
+
+		return
+	}
+	log.Printf("Item found: %+v\n", product.ToJSON())
+
+	render.JSON(w, r, product.ToJSON())
+
+}
+func (s *Server) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product ProductResponse
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		log.Println(err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	log.Printf("producto %+v\n", product)
+	tx, err := s.DB.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tx.Exec("INSERT INTO public.product (\"name\", quantity, unit, stores) VALUES($1, $2, $3, $4);", product.Name, product.Quantity, product.Unit, product.Stores)
+	if err != nil {
+		tx.Rollback()
+		log.Fatal("Error during insert: ", err)
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal("Error commiting: ", err)
+	}
+	render.JSON(w, r, product)
+}
+func (s *Server) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	var id = chi.URLParam(r, "id")
+
+	tx, err := s.DB.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tx.Exec("DELETE FROM public.product WHERE id=$1;", id)
+	if err != nil {
+		tx.Rollback()
+		log.Fatal("Error during delete: ", err)
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal("Error commiting: ", err)
+	}
+	w.Write([]byte("Delete successful"))
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hi"))
-}
+func (s *Server) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	var id = chi.URLParam(r, "id")
+	var product ProductResponse
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		log.Println(err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	tx, err := s.DB.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tx.Exec("UPDATE public.product SET \"name\"=$1, quantity=$2, unit=$3, stores=$4 WHERE id=$5;", product.Name, product.Quantity, product.Unit, product.Stores, id)
+	if err != nil {
+		tx.Rollback()
+		log.Fatal("Error during update: ", err)
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal("Error commiting: ", err)
+	}
+	render.JSON(w, r, product)
 
+}
+func (s *Server) PatchProduct(w http.ResponseWriter, r *http.Request) {
+	var id = chi.URLParam(r, "id")
+	var product ProductResponse
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		log.Println(err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	tx, err := s.DB.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var updateClauses []string
+	var args []interface{}
+	argIndex := 1
+	v := reflect.ValueOf(product)
+	t := reflect.TypeOf(product)
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+
+		// Check if the field is a pointer
+		if field.Kind() == reflect.Ptr {
+			// Only check if the pointer is not nil
+			if !field.IsNil() {
+				columnName := fieldToColumnName(t.Field(i)) // Get the corresponding column name from the struct tag
+				fieldValue := field.Elem().Interface()      // Dereference the pointer to get the actual value
+
+				// Add to update clauses
+				updateClauses = append(updateClauses, fmt.Sprintf("%s = $%d", columnName, argIndex))
+				args = append(args, fieldValue)
+				argIndex++
+			}
+		} else {
+			// Handle non-pointer fields directly
+			if !field.IsZero() { // Check if the value is the zero value for its type
+				columnName := fieldToColumnName(t.Field(i))
+				fieldValue := field.Interface() // Get the actual value
+
+				// Add to update clauses
+				updateClauses = append(updateClauses, fmt.Sprintf("%s = $%d", columnName, argIndex))
+				args = append(args, fieldValue)
+				argIndex++
+			}
+		}
+
+	}
+	if len(updateClauses) == 0 {
+		http.Error(w, "no fields for update", http.StatusBadRequest)
+		return
+	}
+	query := fmt.Sprintf("Update product set %s where id=$%d", strings.Join(updateClauses, ", "), argIndex)
+	args = append(args, id)
+	_, err = tx.Exec(query, args...)
+	if err != nil {
+		tx.Rollback()
+		fmt.Println(err)
+		http.Error(w, "Failed to update", http.StatusInternalServerError)
+		return
+	}
+	var updatedProduct Product
+	tx.QueryRow("select * from product where product.id = $1", id).Scan(&updatedProduct.id, &updatedProduct.name, &updatedProduct.quantity, &updatedProduct.unit, &updatedProduct.stores)
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal("Error commiting: ", err)
+	}
+	render.JSON(w, r, updatedProduct.ToJSON())
+
+}
+func fieldToColumnName(field reflect.StructField) string {
+	jsonTag := field.Tag.Get("json")
+	if jsonTag == "" {
+		return field.Name
+	}
+	return strings.Split(jsonTag, ",")[0]
+}
 func connectToPostgres(connectionString string) *sql.DB {
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
